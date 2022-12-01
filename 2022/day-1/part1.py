0b8466b3b12a961d9ef1e698a9cb71a2757c8cf9
Raghav Nair <nairraghav@hotmail.com>
2022-11-30 21:08:36 -0800

    Adding 2022 Day 1 Solutions

--- 2022/day-1/part1.py
+++ 2022/day-1/part1.py
@@ -1,5 +1,15 @@
 puzzle_input_list = []
+current_count = 0
 
 with open("input.txt", "r") as puzzle_input:
+    
     for line in puzzle_input:
-        puzzle_input_list.append(int(line))
+        try:
+            current_count += int(line)
+        except:
+            puzzle_input_list.append(current_count)
+            current_count = 0
+    
+    puzzle_input_list.append(current_count)
+    
+print(max(puzzle_input_list))
---
+++ 2022/day-1/part2.py
@@ -0,0 +1,30 @@
+puzzle_input_list = []
+current_count = 0
+
+with open("input.txt", "r") as puzzle_input:
+    
+    for line in puzzle_input:
+        try:
+            current_count += int(line)
+        except:
+            puzzle_input_list.append(current_count)
+            current_count = 0
+    
+    puzzle_input_list.append(current_count)
+    
+
+# to make a max heap, we use negative values
+puzzle_input_list = [-1 * item for item in puzzle_input_list]
+
+import heapq
+heapq.heapify(puzzle_input_list)
+
+running_sum = 0
+for _ in range(3):
+    current_item = heapq.heappop(puzzle_input_list)
+    running_sum += current_item
+
+# need to cancel out the negative from using the heap
+running_sum = -1 * running_sum
+
+print(running_sum)
