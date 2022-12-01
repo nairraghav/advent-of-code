40e7b83107cfe9d0805b0cb55f4d58d523d5a7ca
Raghav Nair <nairraghav@hotmail.com>
2022-11-30 21:16:40 -0800

    Adding real input and minor formatting changes for solution

--- 2022/day-1/part1.py
+++ 2022/day-1/part1.py
@@ -2,14 +2,14 @@ puzzle_input_list = []
 current_count = 0
 
 with open("input.txt", "r") as puzzle_input:
-    
+
     for line in puzzle_input:
         try:
             current_count += int(line)
-        except:
+        except ValueError:
             puzzle_input_list.append(current_count)
             current_count = 0
-    
+
     puzzle_input_list.append(current_count)
-    
+
 print(max(puzzle_input_list))
--- 2022/day-1/part2.py
+++ 2022/day-1/part2.py
@@ -1,26 +1,27 @@
+import heapq
+
 puzzle_input_list = []
 current_count = 0
 
 with open("input.txt", "r") as puzzle_input:
-    
+
     for line in puzzle_input:
         try:
             current_count += int(line)
-        except:
+        except ValueError:
             puzzle_input_list.append(current_count)
             current_count = 0
-    
+
     puzzle_input_list.append(current_count)
-    
+
 
 # to make a max heap, we use negative values
 puzzle_input_list = [-1 * item for item in puzzle_input_list]
 
-import heapq
 heapq.heapify(puzzle_input_list)
 
 running_sum = 0
-for _ in range(3):
+for _ in range(3):  # only needed top 3
     current_item = heapq.heappop(puzzle_input_list)
     running_sum += current_item
 
