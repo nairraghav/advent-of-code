bd22d3134ef4d6ff6b33f6b5accd17e78c1b1265
Raghav Nair <nairraghav@hotmail.com>
2022-12-02 21:45:43 -0800

    Adding day 3 solution and prepping day 4

--- 2022/day-3/part1.py
+++ 2022/day-3/part1.py
@@ -1,7 +1,28 @@
 puzzle_input_list = []
+running_total = 0
 
 with open("input.txt", "r") as puzzle_input:
 
     for line in puzzle_input:
-        pass
+        rucksack_length = len(line)
+        rucksack_one = line[:rucksack_length//2]
+        rucksack_two = line[rucksack_length//2:]
+
+        unique_letters = {letter for letter in rucksack_one}
+        matched_letter = None
+
+        for letter in rucksack_two:
+            if letter in unique_letters:
+                matched_letter = letter
+                break
+            else:
+                pass
+
+        is_upper = matched_letter.isupper()
+        running_total += ord(matched_letter.lower()) - 96
+
+        if is_upper:
+            running_total += 26
+
+print(running_total)
 
---
+++ 2022/day-3/part2.py
@@ -0,0 +1,31 @@
+puzzle_input_list = []
+running_total = 0
+
+with open("input.txt", "r") as puzzle_input:
+
+    for line in puzzle_input:
+        line = line.strip()
+        puzzle_input_list.append(line)
+        
+
+for index in range(0, len(puzzle_input_list), 3):
+    first_rucksack = puzzle_input_list[index]
+    second_rucksack = puzzle_input_list[index+1]
+    third_rucksack = puzzle_input_list[index+2]
+
+    first_unique_rucksack = {letter for letter in first_rucksack}
+    second_unique_rucksack = {letter for letter in second_rucksack}
+    third_unique_rucksack = {letter for letter in third_rucksack}
+
+    common_values = list({letter for letter in first_unique_rucksack if letter in second_unique_rucksack and letter in third_unique_rucksack})
+    
+    matched_letter = common_values[0]
+    
+    is_upper = matched_letter.isupper()
+    running_total += ord(matched_letter.lower()) - 96
+
+    if is_upper:
+        running_total += 26
+
+print(running_total)
+
