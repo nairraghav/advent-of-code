7b3ededc01b08d1badd8f51ecda12d36cdd4d392
Raghav Nair <nairraghav@hotmail.com>
2022-12-05 21:08:01 -0800

    Adding day 6 solution and stubbing day 7:

---
+++ 2022/day-6/part2.py
@@ -0,0 +1,16 @@
+puzzle_input = list()
+message_marker_length = 14
+result = None
+
+with open("input.txt", "r") as puzzle_input_file:
+    for line in puzzle_input_file:
+        puzzle_input = line
+
+for index in range(len(puzzle_input) - message_marker_length):
+    subset = puzzle_input[index:index + message_marker_length]
+    subset_counter = {letter for letter in subset}
+    if len(subset_counter) == message_marker_length:
+        result = index + message_marker_length
+        break
+
+print(result)
---
+++ 2022/day-7/part1.py
@@ -0,0 +1,8 @@
+puzzle_input_list = list()
+result = None
+
+with open("input.txt", "r") as puzzle_input:
+    for line in puzzle_input:
+        puzzle_input_list.append(line.strip())
+
+print(result)
