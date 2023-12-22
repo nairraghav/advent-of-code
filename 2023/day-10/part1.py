1f3ae15178fbf5c9a588f791dca67dfb46c90fbe
Raghav Nair <nairraghav@hotmail.com>
2023-12-22 15:34:23 -0800

    Adding day 10 part 1 solution

--- 2023/day-10/part1.py
+++ 2023/day-10/part1.py
@@ -17,13 +17,83 @@ def find_starting_point(lines):
 
     return (None, None)
 
+def get_next_coordinate(start_x, start_y, symbol_x, symbol_y, symbol):
+    match symbol:
+        case "|":
+            if symbol_y > start_y:
+                return start_x, start_y + 2
+            else:
+                return start_x, start_y - 2
+        case "-":
+            if symbol_x > start_x:
+                return start_x + 2, start_y
+            else:
+                return start_x - 2, start_y
+        case "L":
+            if symbol_y > start_y:
+                return start_x + 1, start_y + 1
+            else:
+                return start_x - 1, start_y - 1
+        case "J":
+            if symbol_y > start_y:
+                return start_x - 1, start_y + 1
+            else:
+                return start_x + 1, start_y - 1
+        case "7":
+            if symbol_y == start_y:
+                return start_x + 1, start_y + 1
+            else:
+                return start_x - 1, start_y - 1
+        case "F":
+            if symbol_y < start_y:
+                return start_x + 1, start_y - 1
+            else:
+                return start_x - 1, start_y + 1
+        case ".":
+            pass
+
 def get_travel_path(lines, start_x, start_y):
-    pass
+    print(start_x, start_y, "\n")
+    valid_paths = list()
+    if lines[start_y][start_x + 1] in ("-", "J", "7"):
+        valid_paths.append((start_x + 1, start_y))
+
+    if lines[start_y][start_x - 1] in ("-", "L", "F"):
+        valid_paths.append((start_x - 1, start_y))
+
+    if lines[start_y-1][start_x] in ("|", "7", "F"):
+        valid_paths.append((start_x, start_y - 1))
+
+    if lines[start_y+1][start_x] in ("|", "L", "J"):
+        valid_paths.append((start_x, start_y + 1))
+    
+    assert len(valid_paths) == 2
+    path_one, path_two = valid_paths
+    
+    steps = 1
+    old_one_x = start_x
+    old_one_y = start_y
+    old_two_x = start_x
+    old_two_y = start_y
+    while path_one != path_two:
+        print(path_one, lines[path_one[1]][path_one[0]])
+        print(path_two, lines[path_two[1]][path_two[0]])
+        print("\n")
+        temp_one_x, temp_one_y = path_one
+        temp_two_x, temp_two_y = path_two
+        path_one = get_next_coordinate(old_one_x, old_one_y, path_one[0], path_one[1], lines[path_one[1]][path_one[0]])
+        path_two = get_next_coordinate(old_two_x, old_two_y, path_two[0], path_two[1], lines[path_two[1]][path_two[0]])
+        old_one_x, old_one_y = temp_one_x, temp_one_y
+        old_two_x, old_two_y = temp_two_x, temp_two_y
+        steps += 1
+
+    return steps
+
 
 def main():
     lines = get_input()
     start_x, start_y = find_starting_point(lines)
-    path_traveled = get_travel_path(lines, start_x, start_y)
+    print(get_travel_path(lines, start_x, start_y))
 
 
 if __name__ == "__main__":
