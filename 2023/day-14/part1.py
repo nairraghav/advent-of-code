3dc5c96aa07fdca14bd456c3d9041de3bcf645f7
Raghav Nair <nairraghav@hotmail.com>
2023-12-31 16:46:42 -0800

    Adding solutions for day 14 and 15 part 1 only for both

--- 2023/day-14/part1.py
+++ 2023/day-14/part1.py
@@ -4,13 +4,62 @@ def get_input():
         lines = puzzle_input.readlines()
         for line in lines:
             line = line.strip()
-            line = line.split()
+            line = [character for character in line]
             puzzle_lines.append(line)
     
     return puzzle_lines
 
+
+def rotate_lines(lines):
+    rotated_lines = list()
+    for row_index in range(len(lines)):
+        a = ""
+        for column_index in range(len(lines[row_index])):
+            a += lines[column_index][row_index]
+        rotated_lines.append([character for character in a])
+
+    return rotated_lines
+
+
+def row_shift_zeroes_left(line):
+    last_to_check = 0
+    for index in range(len(line)):
+        if line[index] == "O":
+            check_index = index - 1
+            possible_place_index = index
+
+            while check_index >= last_to_check:
+                if line[check_index] in ("O", "#"):
+                    break
+                else:
+                    possible_place_index = check_index
+                check_index -= 1
+
+            if possible_place_index != index:
+                line[possible_place_index], line[index] = line[index], line[possible_place_index]
+
+    return line
+
+    
+
 def main():
     lines = get_input()
+    rotated_lines = rotate_lines(lines)
+
+    for row_index in range(len(rotated_lines)):
+        rotated_lines[row_index] = row_shift_zeroes_left(rotated_lines[row_index])
+
+    lines = rotate_lines(rotated_lines)
+    
+    total_load = 0
+    counter = 1
+    
+    for line in lines[::-1]:
+        number_of_rocks = len([rock for rock in line if rock == "O"]) * counter
+        total_load += number_of_rocks
+        counter += 1
+
+    print(total_load)
 
 
 if __name__ == "__main__":
--- 2023/day-15/part1.py
+++ 2023/day-15/part1.py
@@ -2,15 +2,29 @@ def get_input():
     puzzle_lines = list()
     with open("input.txt", "r") as puzzle_input:
         lines = puzzle_input.readlines()
-        for line in lines:
+        for line in lines[0].split(","):
             line = line.strip()
-            line = line.split()
             puzzle_lines.append(line)
     
     return puzzle_lines
 
+def get_hash(line):
+    running_sum = 0
+    for character in line:
+        running_sum += ord(character)
+        running_sum *= 17
+        running_sum %= 256
+
+    print(running_sum)
+    return running_sum
+
 def main():
     lines = get_input()
+    running_sum = 0
+    for line in lines:
+        running_sum += get_hash(line)
+
+    print(running_sum)
 
 
 if __name__ == "__main__":
