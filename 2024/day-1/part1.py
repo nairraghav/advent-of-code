b5614fc875d229177cf026fffe430bf0315b1757
Raghav Nair <rnair@Raghavs-MacBook-Pro.local>
2024-11-30 18:01:48 -0800

    Adding 2024 day 1 stub:

---
+++ 2024/day-1/part1.py
@@ -0,0 +1,12 @@
+puzzle_input_list = []
+running_sum = 0
+
+with open("input.txt", "r") as puzzle_input:
+    lines = puzzle_input.readlines()
+    puzzle_input_list = [line.strip() for line in lines]
+    
+    
+    for input_line in puzzle_input_list:
+        pass
+
+    print(running_sum)
